48769cb6c98ccaa5d2d075ac4eb7b8c85cc9b1c8
Jake Zarobsky <jzarob@gmail.com>
2017-04-09 19:22:42 -0500

    Added tests and updated app.json

---
+++ tests.py
@@ -0,0 +1,48 @@
+import unittest
+from main import commands
+
+"""
+commands = {
+    'create': create_list,
+    'delete': delete_list,
+    'add': add_item,
+    'rm': remove_item,
+    'sub': add_sub,
+    'unsub': remove_sub,
+    'stop': stop,
+    'help': help
+}
+"""
+
+
+class SMSRemindTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_list(self):
+        pass
+
+    def test_delete_list(self):
+        pass
+
+    def test_add_item(self):
+        pass
+
+    def test_remove_item(self):
+        pass
+
+    def test_add_subscription(self):
+        pass
+
+    def test_remove_subscription(self):
+        pass
+
+    def test_stop(self):
+        pass
+
+    def test_help(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
